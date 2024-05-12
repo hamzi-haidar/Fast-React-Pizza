1e4b848696a35151ca7fbcaa3bd453f28346ae72
Hamzi Haidar <hamzihaider@gmail.com>
2024-05-12 16:36:12 +0300

    first commit

---
+++ src/ui/LinkButton.jsx
@@ -0,0 +1,23 @@
+import { Link, useNavigate } from "react-router-dom";
+
+function LinkButton({ children, to }) {
+  const className = "text-sm text-blue-500 hover:text-blue-600 hover:underline";
+
+  // we are not using these because passing {-1} for the prop in the error component works just fine
+  // const navigate = useNavigate();
+  // if (to === "-1") {
+  //   return (
+  //     <button className={className} onClick={() => navigate(-1)}>
+  //       {children}
+  //     </button>
+  //   );
+  // }
+
+  return (
+    <Link to={to} className={className}>
+      {children}
+    </Link>
+  );
+}
+
+export default LinkButton;
---
+++ src/ui/UpdateItemQuantity.jsx
@@ -0,0 +1,24 @@
+import { useDispatch } from "react-redux";
+import Button from "./button";
+import {
+  decreaseItemQuantity,
+  increaseItemQuantity,
+} from "../features/cart/cartSlice";
+
+function UpdateItemQuantity({ id, currentQuantity }) {
+  const dispatch = useDispatch();
+
+  return (
+    <div className="flex items-center gap-2 md:gap-3">
+      <Button type="round" onClick={() => dispatch(decreaseItemQuantity(id))}>
+        -
+      </Button>
+      <span className="text-sm font-medium">{currentQuantity}</span>
+      <Button type="round" onClick={() => dispatch(increaseItemQuantity(id))}>
+        +
+      </Button>
+    </div>
+  );
+}
+
+export default UpdateItemQuantity;
